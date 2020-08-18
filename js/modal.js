a2cfea848e195d447e4338460997d9aee2fa2072
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-08-18 11:57:32 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,14 +2,12 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    ModalBtn: document.querySelector("[modal-close]"),
     modal: document.querySelector("[data-modal]"),
     body: document.querySelector("[data-body]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.ModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
